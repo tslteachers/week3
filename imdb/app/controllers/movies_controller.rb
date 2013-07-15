51f1c83c7bd561af80eed9f07de82994e400d1fa
Jeff Cohen <jeff@starterleague.com>
2013-07-15 15:25:09 -0500

    Challenge: can you modify the index page to generate links with querystring parameters so that we can display the correct movie title?

--- imdb/app/controllers/movies_controller.rb
+++ imdb/app/controllers/movies_controller.rb
@@ -1,12 +1,15 @@
 class MoviesController < ApplicationController
 
   def index
-    @movies = ['Lincoln', 'Apollo 13', 'Star Wars', 'Raiders of the Lost Ark', 'Fantastic Mr. Fox']
+    @movies = ['Lincoln', 'Apollo 13', 'Star Wars', 'Raiders of the Lost Ark']
 
     render 'index'
   end
 
   def show
+    @movies = ['Lincoln', 'Apollo 13', 'Star Wars', 'Raiders of the Lost Ark']
+    movie_id = params["id"]
+    @title = @movies[movie_id.to_i]
     render 'show'
   end
 
