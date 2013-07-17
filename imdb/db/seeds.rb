98535f030bd1697c1aa35184328071926a84753f
Jeff Cohen <jeff@starterleague.com>
2013-07-17 11:28:45 -0500

    Provide some sample movie data

--- imdb/db/seeds.rb
+++ imdb/db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+list_of_movies = [
+  { title: 'Lincoln', year: 2012 },
+  { title: 'Apollo 13', year: 1995 },
+  { title: 'Star Wars', year: 1977 },
+  { title: 'Raiders of the Lost Ark', year: 1981 }
+]
+
+# Movie.destroy_all
+# list_of_movies.each do |movie_data|
+#   m = Movie.new
+#   m.title = movie_data[:title]
+#   m.year = movie_data[:year]
+#   m.save
+# end
